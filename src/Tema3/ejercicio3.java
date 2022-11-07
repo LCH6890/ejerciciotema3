5046fb77c45bdd334251a8054d11c82d3f0e3770
LCh <lorenachamorro@gmail.com>
2022-11-07 17:40:18 +0100

    se agrega ejercicio tema3

---
+++ src/Tema3/ejercicio3.java
@@ -0,0 +1,11 @@
+package Tema3;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] diasSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+        for(int i= 0;i < diasSemana.length; i++){
+            System.out.println(diasSemana[i]);
+        }
+    }
+}
